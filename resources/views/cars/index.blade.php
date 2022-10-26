70cda8b4da0b8922017e5888706e57ff75535f7b
Fayez Alhajj <fayez.hajjj@gmail.com>
2022-10-26 11:20:27 +0300

    first commit

---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,29 @@
+@extends('cars.layouts.app')
+
+@section('content')
+<div class=" m-auto py-24 w-4/5 ">
+    <div class="text-center">
+        <h1 class="text-5xl uppercase bold" >
+            Cars
+        </h1>
+    </div>
+
+    <div class="w-5/6 py-18">
+        <div class="m-auto">
+            <span class="uppercase text-blue-500 font-bold text-xs italic">
+                Founded:2020
+            </span>
+            <h2 class="text-gray-700 text-5xl">
+                Audi
+            </h2>
+            <p class="text-lg text-gray-700 py-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eos quisquam saepe, aut obcaecati iure voluptates? Molestiae repudiandae quo illo at velit sit accusantium nulla ipsa. Quis, praesentium. Ut, minus.
+            </p>
+
+            <hr class="mt-5 mb-8">
+
+        </div>
+    </div>
+</div>
+
+@endsection
